920f91b22e859b271f974d6d2093fda0863f232d
Evan Lucas <evan@btc.com>
2015-02-17 16:08:23 -0600

    Add tests for client

---
+++ test/fixtures/server.js
@@ -0,0 +1,92 @@
+var http = require('http')
+
+module.exports = http.createServer(handle)
+
+function handle(req, res) {
+  var url = req.url
+  if (req.method === 'PUT') return handlePUT(req, res)
+  if (url === '/api/v1/runners/register.json') {
+    return handleRegister(req, res)
+  }
+
+  var body = ''
+  req.on('data', function(chunk) {
+    body += chunk
+  })
+
+  req.on('end', function() {
+    body = JSON.parse(body)
+    if (body.token === 'biscuits') {
+      res.writeHead(201, {
+        'Content-Type': 'application/json'
+      })
+      res.end(JSON.stringify({
+        id: 1
+      , project_id: 1
+      , commands: 'ls\npwd\n'
+      , repo_url: 'git@github.com:evanlucas/gcr-test'
+      , ref: 'origin/master'
+      , ref_name: 'origin/master'
+      , allow_git_fetch: false
+      , timeout: 5000
+      }))
+    } else if (body.token === 'blah') {
+      res.writeHead(403, {
+        'Content-Type': 'application/json'
+      })
+      res.end()
+    } else {
+      res.writeHead(404, {
+        'Content-Type': 'application/json'
+      })
+      res.end()
+    }
+  })
+}
+
+function handleRegister(req, res) {
+  var body = ''
+  req.on('data', function(chunk) {
+    body += chunk
+  })
+
+  req.on('end', function() {
+    body = JSON.parse(body)
+    if (body.token === 'biscuits') {
+      res.writeHead(201, {
+        'Content-Type': 'application/json'
+      })
+
+      res.end(JSON.stringify({
+        token: 'biscuits'
+      }))
+    } else {
+      res.writeHead(500, {
+        'Content-Type': 'application/json'
+      })
+      res.end()
+    }
+  })
+}
+
+function handlePUT(req, res) {
+  var body = ''
+  req.on('data', function(chunk) {
+    body += chunk
+  })
+
+  req.on('end', function() {
+    body = JSON.parse(body)
+    if (body.trace === 'blah') {
+      res.writeHead(200, {
+        'Content-Type': 'application/json'
+      })
+      res.end()
+    } else {
+      res.writeHead(500, {
+        'Content-Type': 'application/json'
+      })
+      res.end()
+    }
+  })
+}
--- test/gcr.js
+++ test/gcr.js
@@ -8,8 +8,16 @@ var should = require('should')
   , sinon = require('sinon')
 
 describe('gcr', function() {
-  before(function() {
+  var server, port
+  before(function(done) {
     rimraf.sync(__dirname + '/home/.config/gcr.json')
+    rimraf.sync(__dirname + '/home/builds')
+    server = require('./fixtures/server')
+    server.listen(0, function(err) {
+      if (err) return done(err)
+      port = server.address().port
+      done()
+    })
   })
 
   it('should be an EventEmitter', function() {
@@ -45,12 +53,13 @@ describe('gcr', function() {
   it('should allow loading', function(done) {
     gcr.loaded.should.be.false
     gcr.load({
-      url: 'https://gcr'
+      url: 'http://127.0.0.1:' + port
     , token: 'biscuits'
     , buildDir: '/tmp/gcr-builds'
     , npm: true
     , strictSSL: false
     , timeout: 5000
+    , loglevel: 'silent'
     }, function(err) {
       if (err) return done(err)
       gcr.loaded.should.be.true
@@ -72,10 +81,10 @@ describe('gcr', function() {
 
     it('should allow construction', function() {
       build = Build({
-        commands: []
+        commands: ['npm test']
       , timeout: 5000
       , project_id: 1
-      , repo_url: 'git://github.com/evanlucas/gcr.git'
+      , repo_url: 'git://github.com/evanlucas/gcr-test.git'
       , ref: 'origin/master'
       , allow_git_fetch: true
       , before_sha: 'blah'
@@ -93,21 +102,127 @@ describe('gcr', function() {
       build.should.have.property('state', 'waiting')
     })
 
-    it('should run', function(done) {
+    it('should run with clone', function(done) {
+      this.timeout(10000)
       var sandbox = sinon.sandbox.create()
+      sandbox.stub(build, 'update', function(cb) {
+        cb && cb()
+      })
+      var count = 0
+      function next() {
+        count++
+        if (count === 2) done()
+      }
       build.on('done', function(success) {
         success.should.be.true
+        sandbox.restore()
+        done()
       })
       build.run()
       build.state.should.equal('running')
-      sandbox.stub(build.update).callsArgWith(0, function() {
+    })
+
+    it('should run with fetch', function(done) {
+      var build2 = Build({
+        commands: ['npm test']
+      , timeout: 5000
+      , project_id: 2
+      , repo_url: 'git://github.com/evanlucas/gcr-test.git'
+      , ref: 'origin/master'
+      , allow_git_fetch: false
+      , before_sha: 'blah'
+      , ref_name: 'master'
+      , id: 2
+      })
+      this.timeout(10000)
+      var sandbox = sinon.sandbox.create()
+      sandbox.stub(gcr.client, 'updateBuild', function(id, s, o, cb) {
+        cb && cb()
+      })
+      var count = 0
+      function next() {
+        count++
+        if (count === 2) done()
+      }
+      build2.on('done', function(success) {
+        success.should.be.true
+        sandbox.restore()
         done()
       })
+      build2.run()
+      build2.state.should.equal('running')
     })
   })
 
   describe('client', function() {
+    it('should be accessible via gcr.client', function() {
+      should.exist(gcr.client)
+    })
 
+    it('apuUrl() should return gcr.config.get(\'url\')', function() {
+      gcr.client.apiUrl().should.equal(gcr.config.get('url'))
+    })
+
+    describe('updateBuild', function() {
+      it('should return null, true on success', function(done) {
+        gcr.client.updateBuild(1, 'running', 'blah', function(err, out) {
+          if (err) return done(err)
+          out.should.be.true
+          done()
+        })
+      })
+
+      it('should return null, false on error', function(done) {
+        gcr.client.updateBuild(1, 'running', 'blah2', function(err, out) {
+          if (err) return done(err)
+          out.should.be.false
+          done()
+        })
+      })
+    })
+
+    describe('registerRunner', function() {
+      it('should return null, token on success', function(done) {
+        gcr.client.registerRunner(1, 'biscuits', function(err, out) {
+          if (err) return done(err)
+          should.exist(out)
+          done()
+        })
+      })
+
+      it('should return err on error', function(done) {
+        gcr.client.registerRunner(1, 'blah', function(err, out) {
+          should.exist(err)
+          err.should.match(/Invalid response/)
+          done()
+        })
+      })
+    })
+
+    describe('getBuild', function() {
+      it('should return null, object on success', function(done) {
+        gcr.config.set('token', 'biscuits')
+        gcr.client.getBuild(done)
+      })
+
+      it('should return err on 403', function(done) {
+        gcr.config.set('token', 'blah')
+        gcr.client.getBuild(function(err, out) {
+          should.exist(err)
+          err.should.match(/Unable to get builds/)
+          done()
+        })
+      })
+
+      it('should return undefined on anything else', function(done) {
+        gcr.config.set('token', 'baaaaa')
+        gcr.client.getBuild(function(err, out) {
+          if (err) return done(err)
+          should.not.exist(out)
+          done()
+        })
+      })
+    })
   })
 
   describe('runner', function() {
